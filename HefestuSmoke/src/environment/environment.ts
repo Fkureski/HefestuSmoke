50f547f43d47822e603b0a1755d59e25a544ed0b
Your Name <felipepoulos@gmail.com>
2024-06-03 20:42:30 -0300

    Terminou cadastro e login as telas so

---
+++ HefestuSmoke/src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    const firebase: {
+        apiKey: "AIzaSyD33PEPPdv1Ns0C0DY7NLM5RRMaDxsjoOo",
+        authDomain: "hefestusmoke.firebaseapp.com",
+        databaseURL: "https://hefestusmoke-default-rtdb.firebaseio.com",
+        projectId: "hefestusmoke",
+        storageBucket: "hefestusmoke.appspot.com",
+        messagingSenderId: "766571354707",
+        appId: "1:766571354707:web:d6d3d037d2c5e1b7e5c4a6",
+        measurementId: "G-VJ51V0J8VM"
+    }
+};
