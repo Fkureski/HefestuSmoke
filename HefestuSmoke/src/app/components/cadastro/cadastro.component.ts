db350030dd0463beeac1d2dec42104d8c72a835f
Your Name <felipepoulos@gmail.com>
2024-06-13 20:23:03 -0300

    Inicio

--- HefestuSmoke/src/app/components/cadastro/cadastro.component.ts
+++ HefestuSmoke/src/app/components/cadastro/cadastro.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 
-export class CadastroComponent {  }
+export class CadastroComponent { 
+  cadastroForm!: FormGroup;
+
+  constructor (private fb: FormBuilder){
+    this.cadastroForm = this.fb.group({
+      nome: new FormControl ('', [Validators.required]),
+      cpf: new FormControl ('', [Validators.required]),
+      email: new FormControl ('', [Validators.required]),
+      senha: new FormControl ('', [Validators.required]),
+      dataNascimento: new FormControl ('', [Validators.required])
+    })
+  }
+
+  onSubmit(){
+    if (this.cadastroForm.valid){
+      console.log(this.cadastroForm.value);
+    } else {
+      this.cadastroForm.markAllAsTouched();
+    }
+  }
+  
+ }
