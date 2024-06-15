bc96c841236ba0df41261781ef801610bbc41485
Fleuck <leuckfelipe@outlook.com>
2024-06-15 18:30:45 -0300

    referenciaRoute

--- HefestuSmoke/src/app/app.routes.ts
+++ HefestuSmoke/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent},
-    { path: 'cadastro', component: CadastroComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'cadastro', component: CadastroComponent },
+    { path: 'produtos', component: ProdutosComponent }
 ];
