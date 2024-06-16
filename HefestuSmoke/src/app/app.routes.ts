f3e89799969420e185b372f91a696fbd5aa6c272
Your Name <felipepoulos@gmail.com>
2024-06-16 13:59:27 -0300

    Criei componente de venda

--- HefestuSmoke/src/app/app.routes.ts
+++ HefestuSmoke/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { CadastroProdutoComponent } from './components/cadastro-produto/cadastro-produto.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { VendasComponent } from './components/vendas/vendas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'cadastro', component: CadastroComponent}
+    { path: 'cadastro', component: CadastroComponent},
+    { path: 'cadastroProduto', component: CadastroProdutoComponent},
+    { path: 'produtos', component: ProdutosComponent},
+    { path: 'vendas', component: VendasComponent},
 ];
---
+++ HefestuSmoke/src/app/components/vendas/vendas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-vendas',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './vendas.component.html',
+  styleUrl: './vendas.component.scss'
+})
+export class VendasComponent {
+
+}
